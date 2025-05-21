1766d059e2e2d7494a2001218a299c9f19263243
Abel Nguendia <56958137+abelnguendia18@users.noreply.github.com>
2025-05-21 14:16:52 +0200

    code refactoring

--- app/src/androidTest/java/com/tapondjou/mynotesapp/FirstAppiumTest.java
+++ app/src/androidTest/java/com/tapondjou/mynotesapp/FirstAppiumTest.java
@@ -4,10 +4,8 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 
